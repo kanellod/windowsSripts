74d1bdc0ca734c76f4d8ba3382b68a440ac99311
DK <11782979+kanellod@users.noreply.github.com>
2022-11-22 16:41:27 +0000

    Update runMyWindowsApp.bat

--- runMyWindowsApp.bat
+++ runMyWindowsApp.bat
@@ -15,22 +15,40 @@ echo this should not have happened :(
 echo OFF
 CALL :mainDisplay 
 SET /P input=Please enter a value: 
-if %input% == 0 CALL :busyCTRL
-if %input% == 1 CALL :triggerPCStats
-if %input% == 2 CALL :triggerNumLock
-if %input% == 3 CALL :checkDiskStatusHealth
-if %input% == 9 EXIT
+if %input% == 1  CALL :busyCTRL
+if %input% == 2  CALL :triggerPCStats
+if %input% == 3  CALL :triggerNumLock
+if %input% == 4  CALL :checkDiskStatusHealth
+if %input% == 5  CALL :getPCISlotInfo
+if %input% == 6  CALL :getOSVersion
+if %input% == 7  CALL :getExecutablesOfAllProcess
+if %input% == 8  CALL :getProductId
+if %input% == 9  CALL :getBiosVersion
+if %input% == 10 CALL :getprocessIdByEditingthis
+if %input% == 11 CALL :getNumberOfCores
+if %input% == 12 CALL :getListOfInstalledApplication
+if %input% == 13 CALL :getBiosSerial
+if %input% == 0 EXIT
 EXIT /B 0
 
 :mainDisplay 
 echo OFF
 echo Today is %DATE%
 echo Choose from the following options:
-echo [0] keep windows busy with CTRL button
-echo [1] PC stats
-echo [2] numLockTrigger
-echo [3] checkDiskStatusHealth
-echo [9] terminate
+echo [1]  keep windows busy with CTRL button
+echo [2]  PC stats
+echo [3]  numLockTrigger
+echo [4]  checkDiskStatusHealth
+echo [5]  getPCISlotInfo
+echo [6]  getOSVersion
+echo [7]  getExecutablesOfAllProcess
+echo [8]  getProductId
+echo [9]  getBiosVersion
+echo [10] getprocessIdByEditingthis
+echo [11] getNumberOfCores
+echo [12] getListOfInstalledApplication
+echo [13] getBiosSerial
+echo [0] terminate
 EXIT /B 0
 
 :busyCTRL
@@ -47,4 +65,40 @@ EXIT /B 0
 
 :checkDiskStatusHealth
 START CALL checkDiskHealthAndStatus.bat
+EXIT /B 0
+
+:getPCISlotInfo
+START CALL getPCISlotInfo.bat
+EXIT /B 0
+
+:getOSVersion
+START CALL getOSVersion.bat
+EXIT /B 0
+
+:getExecutablesOfAllProcess
+START CALL getExecutablesOfAllProcess.bat
+EXIT /B 0
+
+:getProductId
+START CALL getProductId.bat
+EXIT /B 0
+
+:getBiosVersion
+START CALL getBiosVersion.bat
+EXIT /B 0
+
+:getprocessIdByEditingthis
+START CALL getprocessIdByEditingthis.bat
+EXIT /B 0
+
+:getNumberOfCores
+START CALL getNumberOfCores.bat
+EXIT /B 0
+
+:getListOfInstalledApplication
+START CALL getListOfInstalledApplication.bat
+EXIT /B 0
+
+:getBiosSerial
+START CALL getBiosSerial.bat
 EXIT /B 0
